fafa05ef742dcb5dda68bffe60621a315e1f5a40
SHUBHAM SONUNE <99605005+shubzcode@users.noreply.github.com>
2026-09-07 18:38:13 +0530

    Initial website

---
+++ js/script.js
@@ -0,0 +1,7 @@
+console.log("DevOps Cloud Platform loaded successfully.");
+
+const statusDot = document.querySelector(".status-dot");
+
+if (statusDot) {
+    console.log("Application status: ONLINE");
+}
